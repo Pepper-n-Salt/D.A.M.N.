de8d651ceaee0af11fa2d8f691575f8d66ecf48e
imkehoegden <imke.hoegden@posteo.de>
2026-07-22 14:57:01 +0200

    Feat: add contact page translations

--- frontend/src/i18n/resources.ts
+++ frontend/src/i18n/resources.ts
@@ -3,10 +3,12 @@
 import deCommon from "./de/common.json";
 import deHome from "./de/home.json";
 import deAbout from "./de/about.json";
+import deContact from "./de/contact.json";
 
 import enCommon from "./en/common.json";
 import enHome from "./en/home.json";
 import enAbout from "./en/about.json";
+import enContact from "./en/contact.json";
 
 export const resources = {
   de: {
