0c81815f99809b6682b3052172bcf6f018ea9568
imkehoegden <imke.hoegden@posteo.de>
2026-07-25 23:54:27 +0200

    feat: update history Routes

--- backend/routes/historyRoutes.ts
+++ backend/routes/historyRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 // hier middleware checkAuth importieren
-// hier controller importieren
+import { getLatestEntries } from "../controllers/historyController";
 
 const router = express.Router();
 
