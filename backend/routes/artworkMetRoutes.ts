463255863e6aa012e7339fa098ff832760797101
imkehoegden <imke.hoegden@posteo.de>
2026-07-25 16:58:24 +0200

    feat: add artworkMetRoutes structure

---
+++ backend/routes/artworkMetRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+// hier middleware checkAuth importieren
+
+const router = express.Router();
+
+router.get(
+  "/:metArtworkId/details",
+  () => {},
+  () => {}
+);
+
+export default router;
