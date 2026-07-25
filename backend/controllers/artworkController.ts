a2d6df824a9e6997947bdff1431a1939a082ebed
imkehoegden <imke.hoegden@posteo.de>
2026-07-25 18:40:20 +0200

    feat: add artworkController and artworkRoutes structure

---
+++ backend/controllers/artworkController.ts
@@ -0,0 +1,9 @@
+export const showOneArtwork = async (req: Request, res: Response) => {};
+
+export const showAllArtworks = async (req: Request, res: Response) => {};
+
+export const createArtwork = async (req: Request, res: Response) => {};
+
+export const updateArtwork = async (req: Request, res: Response) => {};
+
+export const deleteArtwork = async (req: Request, res: Response) => {};
---
+++ backend/routes/artworkRoutes.ts
@@ -0,0 +1,22 @@
+import express from "express";
+// an dieser Stelle noch die middleware importieren
+import {
+  createArtwork,
+  showOneArtwork,
+  showAllArtworks,
+  updateArtwork,
+  deleteArtwork,
+} from "../controllers/artworkController";
+
+const router = express.Router();
+
+router.get("/:artworkID", () => {}, showOneArtwork);
+
+router.get("/all", () => {}, showAllArtworks);
+
+router.post("/create", () => {}, createArtwork);
+
+router.patch("/update", () => {}, updateArtwork);
+// ist patch hier richtig oder lieber put?
+
+router.delete("/delete", () => {}, deleteArtwork);
