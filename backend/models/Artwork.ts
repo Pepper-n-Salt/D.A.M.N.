6e6f24a3e88e6fe5b58c2dd8648ba37cfbc557ad
imkehoegden <imke.hoegden@posteo.de>
2026-07-26 00:48:22 +0200

    chore: add models index export

--- backend/models/Artwork.ts
+++ backend/models/Artwork.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from "sequelize";
-import db from "../lib/db.ts";
+import db from "../lib/db.js"; // wird später so zur Laufzeit kompiliert
 
 class Artwork extends Model {
   declare id: string;
---
+++ backend/models/index.ts
@@ -0,0 +1,25 @@
+import Artist from "./Artist.ts";
+import ArtistTranslation from "./ArtistTranslation.ts";
+import Artwork from "./Artwork.ts";
+import ArtworkTranslation from "./ArtworkTranslation.ts";
+import ArtworkArtistAssociation from "./ArtworkArtistAssociation.ts";
+import Exhibition from "./Exhibition.ts";
+import ExhibitionTranslation from "./ExhibitionTranslation.ts";
+import Media from "./Media.ts";
+import Organisation from "./Organisation.ts";
+import User from "./User.ts";
+
+export {
+  Artist,
+  ArtistTranslation,
+  Artwork,
+  ArtworkTranslation,
+  ArtworkArtistAssociation,
+  Exhibition,
+  ExhibitionTranslation,
+  Media,
+  Organisation,
+  User,
+};
+
+// durch diese Datei können wir zum Bsp in den Controllern die Inputs vereinfacht schreiben // import { Artist, Artwork } from "../models/index.js";
