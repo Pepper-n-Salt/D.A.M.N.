7cafdad00e4feca3284100f71fc8d7883adeb195
imkehoegden <imke.hoegden@posteo.de>
2026-07-26 00:40:55 +0200

    feat: update model associations and create model ArtworkArtistAssociation

--- backend/models/associations.ts
+++ backend/models/associations.ts
@@ -1,13 +1,13 @@
 import Artist from "./Artist.js";
-import ArtistTranslation from "./ArtistTranslation.js";
-import Artwork from "./Artwork.js";
-import ArtworkTranslation from "./ArtworkTranslation.js";
-import ArtworkArtistAssociation from "./ArtworkArtistAssociation.js"; // was soll das?
+import ArtistTranslation from "./ArtistTranslation.ts";
+import Artwork from "./Artwork.ts";
+import ArtworkTranslation from "./ArtworkTranslation.ts";
+import ArtworkArtistAssociation from "./ArtworkArtistAssociation.ts"; // brauche ich diese wirklich?
 import Exhibition from "./Exhibition.js";
-import ExhibitionTranslation from "./ExhibitionTranslation.js";
-import Media from "./Media.js";
-import Organisation from "./Organisation.js";
-import User from "./User.js";
+import ExhibitionTranslation from "./ExhibitionTranslation.ts";
+import Media from "./Media.ts";
+import Organisation from "./Organisation.ts";
+import User from "./User.ts";
 
 // Organisation
 
