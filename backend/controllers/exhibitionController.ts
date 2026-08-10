1ed7a6eb47c8eb4187e8ec19bc9e2d9f371f03a2
imkehoegden <imke.hoegden@posteo.de>
2026-08-10 14:26:25 +0200

    feat (exhibition): implement createExhibition in exhibitionController

--- backend/controllers/exhibitionController.ts
+++ backend/controllers/exhibitionController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { Exhibition } from "../models";
+import { ExhibitionTranslation } from "../models";
 
 export const showOneExhibition = async (
   req: Request<{ exhibitionId: string }>,
@@ -43,7 +44,63 @@ export const showAllExhibitions = async (req: Request, res: Response) => {
 
 export const createExhibition = async (req: Request, res: Response) => {
   try {
-  } catch (e) {}
+    const {
+      startDate,
+      endDate,
+      openingEvent,
+      specialEvent,
+      closingEvent,
+      primaryColor,
+      secondaryColor,
+      backgroundColor,
+      textColor,
+      headlineFont,
+      textFont,
+      roundness,
+      languageCode,
+      title,
+      subtitle,
+      location,
+      description,
+      slug,
+    } = req.body;
+
+    const exhibition = await Exhibition.create({
+      id: crypto.randomUUID(),
+      startDate,
+      endDate,
+      openingEvent,
+      specialEvent,
+      closingEvent,
+      createdBy: "274da430-60da-4903-b6ac-bf37f1d2853d", // testweise user-id imker eingesetzt // hier noch austauschen, sobald auth-middleware implementiert ist
+      primaryColor,
+      secondaryColor,
+      backgroundColor,
+      textColor,
+      headlineFont,
+      textFont,
+      roundness,
+    });
+
+    const translation = await ExhibitionTranslation.create({
+      exhibitionId: exhibition.id,
+      languageCode,
+      title,
+      subtitle,
+      location,
+      description,
+      slug,
+    });
+
+    return res.status(201).json({
+      exhibition,
+      translation,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      msg: "Server error.",
+    });
+  }
 };
 
 export const updateExhibition = async (req: Request, res: Response) => {
