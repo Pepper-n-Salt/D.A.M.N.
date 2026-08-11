2907aa2c52af95035ecdd1d97527fc2f85dbf60a
imkehoegden <imke.hoegden@posteo.de>
2026-08-11 23:20:12 +0200

    feature (artwork): implement showOneArtwork

--- backend/controllers/artworkController.ts
+++ backend/controllers/artworkController.ts
@@ -16,9 +16,24 @@ export const showAllArtworks = async (req: Request, res: Response) => {
 };
 
 // Einzelnes Artwork abrufen
-export const showOneArtwork = async (req: Request, res: Response) => {
+export const showOneArtwork = async (
+  req: Request<{ artworkId: string }>,
+  res: Response
+) => {
   try {
-  } catch (e) {}
+    const { artworkId } = req.params;
+
+    const singleArtwork = Artwork.findByPk(artworkId);
+
+    if (!singleArtwork) {
+      return res.status(404).json({ msg: "Artwork wurde nicht gefunden." });
+    }
+    return res.status(200).json(singleArtwork);
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({ msg: "Server-Fehler." });
+  }
 };
 
 export const createArtwork = async (req: Request, res: Response) => {
