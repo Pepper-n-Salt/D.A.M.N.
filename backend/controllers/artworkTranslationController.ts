8aa23ba73ffabccfd688c011f55a928da8679814
imkehoegden <imke.hoegden@posteo.de>
2026-08-12 00:03:40 +0200

    Feat (artworkTranslation): implement basic structure routes and controller

---
+++ backend/controllers/artworkTranslationController.ts
@@ -0,0 +1,9 @@
+export const createArtworkTranslation = async (req: Request, res: Response) => {
+  try {
+  } catch (e) {}
+};
+
+export const updateArtworkTranslation = async (req: Request, res: Response) => {
+  try {
+  } catch (e) {}
+};
