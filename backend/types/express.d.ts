6237e064cdd61319418d754c1c5d5497ef438d6b
imkehoegden <imke.hoegden@posteo.de>
2026-08-11 16:32:37 +0200

    refactor: add global typing for authenticated user

--- backend/types/express.d.ts
+++ backend/types/express.d.ts
@@ -1,11 +1,10 @@
+import type { AuthUser } from "./auth.types.js";
+// global gesetzte Typ-Erweiterungen müssen nicht nochmal in die jeweiligen Datei importiert werden, also hier nicht noch in die checkAuth middleware
 declare global {
   namespace Express {
+    // Declaration Merging
     interface Request {
-      user?: {
-        id: string;
-        organisationId: string;
-        role: "super" | "admin" | "user";
-      };
+      user?: AuthUser;
     }
   }
 }
