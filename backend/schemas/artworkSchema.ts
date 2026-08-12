7e44a3741cf6f8a605b5864bb2031e88955790c9
imkehoegden <imke.hoegden@posteo.de>
2026-08-12 16:31:05 +0200

    feat (artwork): implement validate zod middleware in routes

--- backend/schemas/artworkSchema.ts
+++ backend/schemas/artworkSchema.ts
@@ -5,12 +5,12 @@ export const artworkIdSchema = z.object({
 });
 
 export const createArtworkSchema = z.object({
-  year: z.number(),
+  year: z.number().int().min(1),
   country: z.string().max(100).nullable(),
   dimensions: z.string().max(255).nullable(),
   imageId: z.uuid(),
   languageCode: z.string().min(2).max(7),
-  title: z.string().max(255),
+  title: z.string().min(1).max(255),
   subtitle: z.string().max(255).nullable(),
   origin: z.string().max(255).nullable(),
   material: z.string().max(255).nullable(),
@@ -18,25 +18,14 @@ export const createArtworkSchema = z.object({
 });
 
 export const updateArtworkSchema = z.object({
-  year: z.number().optional(),
+  year: z.number().int().min(1).optional(),
   country: z.string().max(100).nullable().optional(),
   dimensions: z.string().max(255).nullable().optional(),
   imageId: z.uuid().optional(),
-  languageCode: z.string().min(2).max(7), // hier nochmal nachschauen
-  title: z.string().max(255).optional(),
+  languageCode: z.string().min(2).max(7), // ist es sinnvoll hier drin oder später über url parameter validieren?
+  title: z.string().min(1).max(255).optional(),
   subtitle: z.string().max(255).nullable().optional(),
   origin: z.string().max(255).nullable().optional(),
   material: z.string().max(255).nullable().optional(),
   description: z.string().nullable().optional(),
 });
-
-// export const updateExhibitionSchema = z.object({
-//   coverImageId: z.uuid().nullable().optional(),
-//   startDate: z.iso.date().optional(),
-//   endDate: z.iso.date().optional(),
-//   languageCode: z.string().min(2).max(7),
-//   title: z.string().min(1).max(255).optional(),
-//   subtitle: z.string().max(255).nullable().optional(),
-//   location: z.string().max(255).nullable().optional(),
-//   description: z.string().nullable().optional(),
-// });
