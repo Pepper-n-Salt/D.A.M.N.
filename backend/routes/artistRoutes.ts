c9493477b9c5af5ef48de94276ba98d5b5ae0021
imkehoegden <imke.hoegden@posteo.de>
2026-08-13 12:28:54 +0200

    feat (artist): insert validation into routes

--- backend/routes/artistRoutes.ts
+++ backend/routes/artistRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { checkAuth } from "../middleware/checkAuth.js";
+import { validateBody, validateParams } from "../middleware/validate.js";
 import {
   showOneArtist,
   showAllArtists,
@@ -7,19 +8,41 @@ import {
   updateArtist,
   deleteArtist,
 } from "../controllers/artistController.js";
+import {
+  artistIdSchema,
+  createArtistSchema,
+  artistLanguageParamsSchema,
+  updateArtistSchema,
+} from "../schemas/artistSchema.js";
 
 const router = express.Router();
 
 router.get("/", checkAuth, showAllArtists);
 
-router.get("/:artistId", checkAuth, showOneArtist); // hier noch validateParams(artistIdSchema) rein
+router.get(
+  "/:artistId",
+  checkAuth,
+  validateParams(artistIdSchema),
+  showOneArtist
+);
 
-router.post("/", checkAuth, createArtist); // validateBody(createArtistSchema)
+router.post("/", checkAuth, validateBody(createArtistSchema), createArtist);
 
 // Soft Delete
-router.patch("/:artistId", checkAuth, deleteArtist); // validateParams(artistIdSchema)
+router.patch(
+  "/:artistId",
+  checkAuth,
+  validateParams(artistIdSchema),
+  deleteArtist
+);
 
 // Artist inkl. Translation aktualisieren
-router.patch("/:artistId/:languageCode", checkAuth, updateArtist); // validateParams(artistLanguageParamsSchema)
+router.patch(
+  "/:artistId/:languageCode",
+  checkAuth,
+  validateParams(artistLanguageParamsSchema),
+  validateBody(updateArtistSchema),
+  updateArtist
+);
 
 export default router;
