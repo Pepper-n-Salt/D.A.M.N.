0369c212492811cbdb84d678ebc8ead6e3c78451
imkehoegden <imke.hoegden@posteo.de>
2026-08-14 11:50:14 +0200

    feat (cloudinary): implement media routes into server file

--- backend/controllers/mediaController.ts
+++ backend/controllers/mediaController.ts
@@ -1,6 +1,15 @@
 import type { Request, Response } from "express";
+import multer from "multer"; // middleware für dateiupload in express
+import cloudinary from "../lib/cloudinary.js";
+import https from "https"; // bringt node schon mit
 
-export const uploadMedia = async (req: Request, res: Response) => {
+export const uploadMedia = async (req: Request<file: string>, res: Response) => {
   try {
+    const name = Date.now();
+
+    // Multer hängt das Bild unter req.file ein
+    if (!req.file) {
+      return res.status(400).json({ msg: "No image uploaded!" });
+    }
   } catch (e) {}
 };
--- backend/server.ts
+++ backend/server.ts
@@ -5,18 +5,19 @@ import cookieParser from "cookie-parser";
 import db from "./lib/db.js";
 import "./models/associations.js";
 
-import authRouter from "./routes/authRoutes.js";
-import userRouter from "./routes/userRoutes.js";
-import historyRouter from "./routes/historyRoutes.js";
-import exhibitionRouter from "./routes/exhibitionRoutes.js";
-import exhibitionTranslationRouter from "./routes/exhibitionTranslationRoutes.js";
-import artworkRouter from "./routes/artworkRoutes.js";
-import artworkTranslationRouter from "./routes/artworkTranslationRoutes.js";
-import artistRouter from "./routes/artistRoutes.js";
-import artistTranslationRouter from "./routes/artistTranslationRoutes.js";
-import metArtworkRouter from "./routes/metArtworkRoutes.js";
-import aiRouter from "./routes/aiRoutes.js";
-import contactRouter from "./routes/contactRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import historyRoutes from "./routes/historyRoutes.js";
+import exhibitionRoutes from "./routes/exhibitionRoutes.js";
+import exhibitionTranslationRoutes from "./routes/exhibitionTranslationRoutes.js";
+import artworkRoutes from "./routes/artworkRoutes.js";
+import artworkTranslationRoutes from "./routes/artworkTranslationRoutes.js";
+import artistRoutes from "./routes/artistRoutes.js";
+import artistTranslationRoutes from "./routes/artistTranslationRoutes.js";
+import metArtworkRoutes from "./routes/metArtworkRoutes.js";
+import aiRoutes from "./routes/aiRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+import mediaRoutes from "./routes/mediaRoutes.js";
 
 const PORT = process.env.PORT || 3000;
 const ORIGIN = process.env.ORIGIN;
@@ -30,22 +31,24 @@ app.use(
   })
 );
 
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // Route Prefix + Routes einbinden
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRouter);
-app.use("/api/history", historyRouter);
-app.use("/api/exhibition", exhibitionRouter);
-app.use("/api/exhibitiontranslation", exhibitionTranslationRouter);
-app.use("/api/artwork", artworkRouter);
-app.use("/api/artworktranslation", artworkTranslationRouter);
-app.use("/api/artist", artistRouter);
-app.use("/api/artisttranslation", artistTranslationRouter);
-app.use("/api/metartwork", metArtworkRouter);
-app.use("/api/ai", aiRouter);
-app.use("/api/contact", contactRouter);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/history", historyRoutes);
+app.use("/api/exhibition", exhibitionRoutes);
+app.use("/api/exhibitiontranslation", exhibitionTranslationRoutes);
+app.use("/api/artwork", artworkRoutes);
+app.use("/api/artworktranslation", artworkTranslationRoutes);
+app.use("/api/artist", artistRoutes);
+app.use("/api/artisttranslation", artistTranslationRoutes);
+app.use("/api/metartwork", metArtworkRoutes);
+app.use("/api/ai", aiRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/media", mediaRoutes);
 
 async function startServer() {
   await db.authenticate(); // prüft die Verbindung von Sequelize zur DB
