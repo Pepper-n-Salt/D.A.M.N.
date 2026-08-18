d2194e5187b477992c138293d9265d9cec16d799
imkehoegden <imke.hoegden@posteo.de>
2026-08-18 10:52:58 +0200

    fix (artist): add medium to model

--- backend/models/Artist.ts
+++ backend/models/Artist.ts
@@ -2,6 +2,7 @@ import { DataTypes, Model } from "sequelize";
 import db from "../lib/db.js";
 import ArtistTranslation from "./ArtistTranslation.js";
 import User from "./User.js";
+import Media from "./Media.js";
 
 class Artist extends Model {
   declare id: string;
@@ -14,6 +15,7 @@ class Artist extends Model {
 
   declare ArtistTranslations?: ArtistTranslation[];
   declare creator?: User;
+  declare Medium?: Media;
 }
 
 Artist.init(
