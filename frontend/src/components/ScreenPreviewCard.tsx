af9170755fd4a3fd002cc2426665e6ce655d055a
imkehoegden <imke.hoegden@posteo.de>
2026-08-19 18:05:23 +0200

    styling (screen): add styling screenpreviewcard

--- frontend/src/components/ScreenPreviewCard.tsx
+++ frontend/src/components/ScreenPreviewCard.tsx
@@ -10,12 +10,12 @@ export default function ScreenPreviewCard({
   uri,
 }: ScreenPreviewCardProps) {
   return (
-    <div>
-      <div>
+    <div className="overflow-hidden border border-black">
+      <div className="aspect-video w-full overflow-hidden bg-neutral-100">
         <iframe src={uri} title={title} className="h-full w-full border-0" />
       </div>
 
-      <div>
+      <div className="p-4">
         <H3>{title}</H3>
       </div>
     </div>
