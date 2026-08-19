7f8939d6d7d3c8a5b527ca0a9cd00923331ecad8
imkehoegden <imke.hoegden@posteo.de>
2026-08-19 18:00:21 +0200

    feat (screen): extend basic structure screenpreviewcard

--- frontend/src/components/ScreenPreviewCard.tsx
+++ frontend/src/components/ScreenPreviewCard.tsx
@@ -1,7 +1,10 @@
+import H3 from "./ui/typography/H3";
+
 interface ScreenPreviewCardProps {
   title: string;
   uri: string;
 }
+
 export default function ScreenPreviewCard({
   title,
   uri,
@@ -10,6 +13,10 @@ export default function ScreenPreviewCard({
     <div>
       <div>
         <iframe src={uri} title={title} className="h-full w-full border-0" />
+      </div>
+
+      <div>
+        <H3>{title}</H3>
       </div>
     </div>
   );
