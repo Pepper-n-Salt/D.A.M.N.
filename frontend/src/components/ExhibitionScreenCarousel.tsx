4730a9c47eb49a16860adf98bdcedcce494aa198
imkehoegden <imke.hoegden@posteo.de>
2026-08-19 19:03:10 +0200

    feat (screen): implement basic structure exhibitionscreencarousel

---
+++ frontend/src/components/ExhibitionScreenCarousel.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import ScreenPreviewCard from "./ScreenPreviewCard";
+import Carouselbutton from "./ui/buttons/Carouselbutton";
+
+import type { CreateExhibitionResponse } from "../api/exhibitionApi";
+
+interface ExhibitionScreenCarouselProps {
+  exhibitions: CreateExhibitionResponse[];
+}
+
+export default function ExhibitionScreenCarousel({
+  exhibitions,
+}: ExhibitionScreenCarouselProps) {
+  const screens = exhibitions.filter((exh) => {
+    exh.isScreen === true;
+  });
+
+  const [startIndex, setStartIndex] = useState(0);
+  const visibleScreens = screens.slice(startIndex, startIndex + 3);
+
+  return (
+    <>
+      <div className="space-y-8">
+        <div className="grid gap-4 lg:grid-cols-3">
+          {/* <ScreenPreviewCard ></ScreenPreviewCard> */}
+        </div>
+      </div>
+
+      <div>
+        <Carouselbutton>← Previous</Carouselbutton>
+        <Carouselbutton>Next →</Carouselbutton>
+      </div>
+    </>
+  );
+}
