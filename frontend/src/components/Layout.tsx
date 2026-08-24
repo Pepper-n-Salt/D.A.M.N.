4ec467e136f30380c68e1f1afc24454edc81c93e
imkehoegden <imke.hoegden@posteo.de>
2026-08-24 11:04:12 +0200

    fix (delete): fix delete for user / admin

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -7,10 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col bg-white text-black">
+    <div className="min-h-screen bg-white text-black">
       <Header />
 
-      <main className="flex-1 px-8 py-24">{children}</main>
+      <main className="min-h-screen bg-red-100 px-8 py-24">{children}</main>
+      {/* <main className="min-h-[75vh] flex-1 px-8 py-24"> */}
 
       <Footer />
     </div>
